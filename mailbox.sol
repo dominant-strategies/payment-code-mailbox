e07cf1cdcd6460b94d3bd3a44700fee4da3e0b75
rileystephens28 <riley.stephens28@gmail.com>
2024-10-03 10:50:14 -0500

    Remove registration functionality

--- mailbox.sol
+++ mailbox.sol
@@ -3,16 +3,12 @@ pragma solidity ^0.8.19;
 
 /**
  * @title PaymentChannelMailbox
- * @notice This contract allows users to register to receive messages via a mailbox,
- *         send notifications to other users, and retrieve notification messages.
- *         It acts as a substitution for BIP47 notification transactions and provides
- *         a backup mechanism for users to retrieve their payment channel info in case of 
- *         a full wallet recovery.
+ * @notice This contract allows users to send notifications to other users, and retrieve 
+ *         notification messages. It acts as a substitution for BIP47 notification transactions 
+ *         and provides a backup mechanism for users to retrieve their payment channel info in case 
+ *         of a full wallet recovery.
  */
 contract PaymentChannelMailbox {
-
-    /// Track registered payment codes
-    mapping(string => bool) public registeredPaymentCodes;
 
     /// Track if a sender has already notified a recipient
     mapping(string => mapping(string => bool)) private hasNotified;
@@ -28,15 +24,6 @@ contract PaymentChannelMailbox {
     event NotificationSent(string senderPaymentCode, string receiverPaymentCode);
 
     /**
-     * @notice Registers the sender to receive messages via this mailbox contract
-     * @param paymentCode The payment code of the sender
-     */
-    function register(string memory paymentCode) public {
-        // Store the payment code associated with the sender's address
-        registeredPaymentCodes[paymentCode] = true;
-    }
-
-    /**
      * @notice Sends a notification message to the recipient
      * @dev Must fail if the sender has already notified this recipient
      *      Creates an entry in the recipient's mailbox containing the sender's payment code
@@ -45,9 +32,6 @@ contract PaymentChannelMailbox {
      * @param receiverPaymentCode The receiver's payment code
      */
     function notify(string memory senderPaymentCode, string memory receiverPaymentCode) public {
-        // Check if sender is registered
-        require(registeredPaymentCodes[senderPaymentCode], "Sender not registered");
-        
         // Check if sender has already notified the recipient
         require(!hasNotified[receiverPaymentCode][senderPaymentCode], "Sender has already notified this recipient");
       
